03a121bd1d411d591e67a26ed0fcef8b6516d698
RoGalletti <160795760+RoGalletti@users.noreply.github.com>
2025-01-26 20:23:18 -0300

    carga de filtro

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-document.addEventListener("DOMContentLoaded", () =>{
-const productList = document.getElementById("productList");
-const categoryFilter = document.getElementById("categoryFilter");
-const viewCartButton = document.getElementById("viewCartButton");
+document.addEventListener("DOMContentLoaded", () => {
+  const productList = document.getElementById("productList");
+  const categoryFilter = document.getElementById("categoryFilter");
+  const viewCartButton = document.getElementById("viewCartButton");
 
-let products= [];
-let cart = [];
+  let products = [];
+  let cart = [];
 
-fetch("https://fakestoreapi.com/products")
-    .then((res)=> res.json())
-    .then((data)=>{
-        products=data;
-        
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) => {
+      products = data;
+      loadCategories(products);
     })
     .catch((error) => console.error("Error cargando productos: ", error));
 
+  function loadCategories(products) {
+    const categories = [
+      ...new Set(products.map((products) => products.category)),
+    ];
+
+    categories.forEach((category) =>{
+        const option = document.createElement("option");
+        option.value = category;
+        option.textContent = category.replace(/^\w/, (c) => c.toUpperCase());
+
+        categoryFilter.appendChild(option);
+    })
+  }
 });
